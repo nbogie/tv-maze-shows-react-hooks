0ba411cf5d09605cd1ee24ebc7afff5e3ca82173
Glitch (starter-react-template) <none>
2020-04-09 02:36:43 +0000

    🍓🦄 Checkpoint ./src/index.css:3234086/640 ./src/App.js:3234086/4114 ./src/index.js:3234086/1682 ./src/logo.svg copy:3234086/2671

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -26,3 +25,4 @@ class App extends Component {
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function PersonCard(props){
+  return (<div className='person-card'><h1>{props.name}!</h1>
+      <div className='card-section'>Address</div>
+      <p>Stuff about {props.name}...</p>
+      <div className='card-section'>Employment</div>
+      <p>More stuff about {props.name}...</p>
+    </div>);
+}
+
+
+const child = (
+  <div>
+    Hello, world!
+    <Example/>
+    <PersonCard name='Alice'></PersonCard>
+    <PersonCard name='Bob'></PersonCard>
+    <PersonCard name='Craig'></PersonCard>
+    <PersonCard name='Dan'></PersonCard>
+    <PersonCard name='Eve'></PersonCard>
+  </div>);
+
+
+
+
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+const parentElem = document.getElementById('root');
+
+ReactDOM.render(child, parentElem);
