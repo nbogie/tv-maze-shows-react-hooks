776ba34d7e0874683ed8ed4c368d5469cdb8a3d5
Glitch (starter-react-template) <none>
2020-04-09 03:18:29 +0000

    🌕🎫 Checkpoint ./src/PalettesList.js:3234086/566 ./src/PalettesList.css:3234086/31

--- src/PalettesList.js
+++ src/PalettesList.js
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./PalettesList.css";
 
-
-
 function PalettesList(props) {
   const [data, setData] = useState({ hits: [] });
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        "https://anapioficeandfire.com/api/characters/"
+        "https://api.tvmaze.com/shows"
       );
       setData({hits: result.data});
     };
@@ -17,16 +15,14 @@ function PalettesList(props) {
   }, []);
 
   
-  console.log(typeof data);
-  console.log(JSON.stringify(data, null, 2));
-  
   return (
     <div className="PalettesList">
       <h2>Palettes List</h2>
       <ul>
         {data.hits.map(item => (
-          <li key={item.id}>
-            <a href={item.url}>{item.aliases[0]} ({item.culture})</a>
+          <li key={item.id} class='show'>
+            {item.id} - <a href={item.url}>{item.name}</a> Rated: {item.rating && item.rating.average}  ({item.genres && item.genres.join(",")})
+            <p>{item.summary}</p>
           </li>
         ))}
       </ul>
