85c9ae9f8117ecf8aa5bc4b8fb8377c9e8a68dac
Glitch (starter-react-template) <none>
2020-04-09 12:46:14 +0000

    👏🌇 Checkpoint ./src/App.js:3234086/1370 ./src/index.css:3234086/161 ./src/App.css:3234086/298 ./src/TVShowList.css:3234086/6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Component, useState} from 'react';
-import TVShowList from './TVShowList';
-import PersonCard from './PersonCard';
-import StateExample from './StateExample';
+import React, { Component, useState } from "react";
+import TVShowList from "./TVShowList";
+import PersonCard from "./PersonCard";
+import StateExample from "./StateExample";
 
-import './App.css';
-
-
+import "./App.css";
 
 const App = (
-  <div>
-    <StateExample/>
-    <TVShowList/>
-    <PersonCard name='Alice'></PersonCard>
-    <PersonCard name='Bob'></PersonCard>
-    <PersonCard name='Craig'></PersonCard>
-    <PersonCard name='Dan'></PersonCard>
-    <PersonCard name='Eve'></PersonCard>
-  </div>);
-
+  <div id="app">
+    <header><h1>This is the header</h1></header>
+    <div id="container">
+      <StateExample />
+      <TVShowList />
+      <PersonCard name="Alice"></PersonCard>
+      <PersonCard name="Bob"></PersonCard>
+      <PersonCard name="Craig"></PersonCard>
+      <PersonCard name="Dan"></PersonCard>
+      <PersonCard name="Eve"></PersonCard>
+    </div>
+  </div>
+);
 
 export default App;
-
